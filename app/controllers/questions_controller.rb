36fd21a8dbbea646985150d7edfa83fe4423b066
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-01-29 13:58:01 +0600

    Fix bug in questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -36,7 +36,7 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
+    @user = User.find_by_nickname(params[:user_id])
     @question = Question.new(user: @user)
   end
 
