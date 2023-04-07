4f295f9510b824fdd00f677c5770e9a7c6602d7a
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-04-07 18:44:07 +0600

    Add Hasgtags and QuestionHashtags

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,9 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
+  has_many :question_hashtags, dependent: :destroy
+  has_many :hashtags, through: :question_hashtags
+  
   validates :body, presence: true
   validates :body, length: { maximum: 280 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_120129) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_122858) do
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "question_hashtags", force: :cascade do |t|
+    t.integer "question_id", null: false
+    t.integer "hashtag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hashtag_id"], name: "index_question_hashtags_on_hashtag_id"
+    t.index ["question_id"], name: "index_question_hashtags_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -35,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_120129) do
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
+  add_foreign_key "question_hashtags", "hashtags"
+  add_foreign_key "question_hashtags", "questions"
 end
