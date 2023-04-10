a20357f9700af7dea6709ee5c44b1a2932460c2f
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-04-10 12:18:12 +0600

    Add hashtag functionality

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,8 @@
 class Hashtag < ApplicationRecord
   has_many :question_hashtags, dependent: :destroy
   has_many :questions, through: :question_hashtags
+
+  scope :with_questions, -> { joins(:questions).distinct }
+
+  REGEX = /#[[:word:]-]+/.freeze
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,18 @@ class Question < ApplicationRecord
 
   has_many :question_hashtags, dependent: :destroy
   has_many :hashtags, through: :question_hashtags
-  
+
   validates :body, presence: true
   validates :body, length: { maximum: 280 }
+
+  after_save_commit :update_hashtags
+
+  private
+
+  def update_hashtags
+    self.hashtags =
+      "#{body} #{answer}".downcase.scan(Hashtag::REGEX).uniq.map do |tag|
+        Hashtag.find_or_create_by(name: tag.delete("#"))
+      end
+  end
 end
